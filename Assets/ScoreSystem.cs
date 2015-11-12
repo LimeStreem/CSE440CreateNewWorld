14a530d1d8fce6151a0d38f02963a76c10a4db08
LimeStreem <LimeStreem@gmail.com>
2015-11-12 10:23:11 -0800

    :sparkles: add ui system

---
+++ Assets/ScoreSystem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ScoreSystem : MonoBehaviour {
+
+	public Transform stageOrigin;
+
+	public Transform player;
+
+	public Text textUI;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		textUI.text = string.Format("{0}m",Mathf.Floor(Mathf.Abs(this.stageOrigin.position.x - this.player.position.x)));
+	}
+}
