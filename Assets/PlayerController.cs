95d8fcc564a32cb421e5ecf148555cb228cbdb60
LimeStreem <LimeStreem@gmail.com>
2015-11-12 10:15:10 -0800

    :sparkles: add audio

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public AudioSource player;
+
+	private float lastY;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -13,5 +17,11 @@ public class PlayerController : MonoBehaviour {
 		var rd = this.GetComponent<Rigidbody2D> ();
 		rd.AddForce (new Vector2 (Input.GetAxis ("Horizontal")*10, 0));
 		if(Input.GetButton("Fire1"))rd.AddForce(new Vector2(0,100));
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if(Mathf.Abs(lastY - this.transform.position.y)>1.0e-1)
+			player.Play ();
+		lastY = transform.position.y;
 	}
 }
